56e8e2a6c8e533779dc78851d3ba6de49bc711c1
nosizi <nosizi@163.com>
2020-08-20 18:51:27 +0800

    chore: added route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
 const app = express()
+
+const PostRoute = require('./routes/post.js')
+
+app.use('/posts', PostRoute)
 
 app.get('/', (req, res) => {
   res.end('Hello World.')
