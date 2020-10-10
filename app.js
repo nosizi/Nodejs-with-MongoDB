675ff090ba763f8c899134603c16e471cd27a918
nosizi <nosizi@163.com>
2020-10-10 22:37:10 +0800

    use cors

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+
 const PostRoute = require('./routes/post.js')
 
+app.use(cors())
 app.use('/posts', PostRoute)
 
 app.get('/', (req, res) => {
+  console.log(req)
+  debugger
   res.end('Hello World.')
 })
 
--- routes/post.js
+++ routes/post.js
@@ -6,4 +6,10 @@ router.get('/', (req, res) => {
   res.end('There is Post page.')
 })
 
+router.post('/', (req, res) => {
+  res.send({
+    title: 'Hello, my man.'
+  })
+})
+
 module.exports = router
