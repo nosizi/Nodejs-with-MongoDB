d68b59342ff2681ab89f1308f2cbfb3461cc79fa
nosizi <nosizi@163.com>
2021-01-25 23:04:05 +0800

    chore: add package

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+const db = mongoose.connect('mongodb://myTester:myTester2021@http://106.53.88.138:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+
+db.on('error', console.error.bind(console, 'MongoDB 连接错误'))
 
 const cors = require('cors')
 
@@ -9,8 +14,6 @@ app.use(cors())
 app.use('/posts', PostRoute)
 
 app.get('/', (req, res) => {
-  console.log(req)
-  debugger
   res.end('Hello World.')
 })
 
